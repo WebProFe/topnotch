c75679e3ef6c69a7e3ccc28fbb8e5fc9a81d6888
WebProFe <medina.techie@gmail.com>
2019-02-07 12:54:08 -0600

    added font size

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 import 'lightbox-react/style.css';
 import NavMenu from './components/Navigation/NavMenu';
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Pages/Home";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Services from './components/Pages/Services';
-import Contact from './components/Pages/Contact'
+import Contact from './components/Pages/Contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 
